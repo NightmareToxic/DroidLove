fde03f5b4f857f084570b2730f40df452aad9a0d
Nightmare Toxic <nightmaretoxic@hotmail.com>
2011-09-02 15:24:25 -0300

    Creating Project DroidLove

---
+++ DroidLove/src/nightmare/droid/love/DroidLoveActivity.java
@@ -0,0 +1,13 @@
+package nightmare.droid.love;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DroidLoveActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
