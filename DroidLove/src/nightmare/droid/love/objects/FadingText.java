2cb83f527d357f29e18871e6d6843f2022601fac
Nightmare Toxic <nightmaretoxic@hotmail.com>
2011-09-06 16:39:18 -0300

    Alterações no Fading Text

--- DroidLove/src/nightmare/droid/love/objects/FadingText.java
+++ DroidLove/src/nightmare/droid/love/objects/FadingText.java
@@ -5,6 +5,7 @@ import javax.microedition.khronos.opengles.GL10;
 import nightmare.droid.love.core.GameObject;
 
 import org.anddev.andengine.entity.modifier.AlphaModifier;
+import org.anddev.andengine.entity.modifier.DelayModifier;
 import org.anddev.andengine.entity.modifier.SequenceEntityModifier;
 import org.anddev.andengine.entity.text.Text;
 import org.anddev.andengine.opengl.font.Font;
@@ -16,9 +17,15 @@ public class FadingText extends GameObject {
 	public FadingText(float px, float py, Font font, String text) {
 		this.pText = new Text(px, py, font, text);
 		pText.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		pText.setColor(1,1,1,0);
+		pText.registerEntityModifier(
+				new SequenceEntityModifier(
+						new DelayModifier(2),
+						new AlphaModifier(1, 0.0f, 1.0f),
+						new DelayModifier(2),
+						new AlphaModifier(1, 1.0f, 0.0f)));
 		this.setEntity(pText);
-		this.getEntity().registerEntityModifier(
-				new SequenceEntityModifier(new AlphaModifier(10, 1.0f, 0.0f)));
+
 	}
 
 }
